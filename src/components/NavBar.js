7607eb5a7934e375ad78134f0caddfc0dc86d320
Jason Williams <jwilliamstap@gmail.com>
2021-05-22 21:54:13 -0400

    adjusted menu and user-welcome message in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,25 +21,23 @@ const Navbar = (props) => {
         <h1 className='display-1 text-dark'> PICK ME UP </h1>
       </header> 
       {user.id ? (
-        <div>
-          <IconContext.Provider value={{ color: 'white' }}>
-            <div className='navbar'>
-              <Link className='menu-bars' to=''> 
-                <FaIcons.FaBars onClick={() => showSidebar()}/> 
-              </Link> 
-            </div>
-            <nav className= {sidebar ? 'nav-menu active' : 'nav-menu'}>       
-              <ul className='nav-menu-items' onClick={() => showSidebar()}>
-                <li className='navbar-toggle'>
-                  <Link className='menu-bars' to=''>
-                    <AiIcons.AiOutlineClose />
-                  </Link>              
-                </li>
-                <Link to=''> <span className="nav-text text-white"> {props.users.single.name} </span></Link>
-                <br />
-                <Link to=''> <img style={{ width:"50px" }} src={props.users.single.photo} alt="profile-image"></img> </Link>
-                {
-                  SideBarData.map((item, index) => {
+        <div className='nav-tabs'>
+          <div className='menu-options-username nav-link btn-danger' onClick={() => showSidebar()}>
+            <IconContext.Provider value={{ color: 'white' }}>
+                <Link className='menu-bars' to=''> 
+                  <FaIcons.FaBars /> 
+                </Link> 
+              <nav className= {sidebar ? 'nav-menu active' : 'nav-menu'}>       
+                <ul className='nav-menu-items' onClick={() => showSidebar()}>
+                  <li className='navbar-toggle'>
+                    <Link className='menu-bars' to=''>
+                      <AiIcons.AiOutlineClose />
+                    </Link>              
+                  </li>
+                  <Link to=''> <span className="nav-text text-white"> {props.users.single.name} </span></Link>
+                  <br />
+                  <Link to=''> <img style={{ width:"50px" }} src={props.users.single.photo} alt="profile-image"></img> </Link>
+                  {SideBarData.map((item, index) => {
                     return (                 
                       <li key={index} className ={item.cName}>                                 
                         <Link to={item.path}>
@@ -50,27 +48,20 @@ const Navbar = (props) => {
                         </Link>
                       </li>
                     )
-                  })
-                }
-              </ul> 
-            </nav>
-          </IconContext.Provider>
-          <div className=' nav nav-tabs justify-content-around'>
-            <button className = "nav-link btn btn-danger text-dark">
-              { user.name ? (
-                <p>Hey { user.name }!</p>
-              ) : (
-                <p>Welcome stranger</p>
-              )}
-            </button>
-            <Link className='nav-link text-dark' to='/'> Home </Link>
-            <button className = "nav-link btn btn-danger text-dark" onClick={() => props.logout(history)}> logout </button>
+                  })}
+                </ul> 
+              </nav>
+            </IconContext.Provider>
+          
+            <button className = "btn"> Hey { user.name }! </button>
           </div>
+          <Link className='nav-link btn btn-danger text-dark' to='/'> Home </Link>
+          <button className = "nav-link btn btn-danger text-dark" onClick={() => props.logout(history)}> Logout </button>
         </div>
       ) : (
-        <div>
-          <Link className='nav-link text-dark' to='/games'> Find a Game </Link>
-          <Link className='nav-link text-dark' to="/login"> Login </Link>
+        <div className=' nav nav-tabs justify-content-around'>
+          <Link className='nav-link btn-danger text-dark' to='/games'> Find a Game </Link>
+          <Link className='nav-link btn-danger text-dark' to="/login"> Login </Link>
         </div>
       )}
     </div>
