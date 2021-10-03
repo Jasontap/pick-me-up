26e1c7ee276101322a8dfaef9a8e18ab650e8d59
Jason Williams <jwilliamstap@gmail.com>
2021-10-03 02:17:41 -0400

    added volleyball for http logging

--- server/api/index.js
+++ server/api/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { static } = express;
 const path = require("path");
 const passport = require("passport");
+const volleyball = require('volleyball');
 
 const app = express();
 module.exports = app;
 
 app.use(express.json());
 app.use(passport.initialize());
+app.use(volleyball);
 
 app.use("/dist", static(path.join(__dirname, "..", "..", "dist")));
 // static file-serving middleware
